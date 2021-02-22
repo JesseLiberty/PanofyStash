8d47c04daca28a9ba8fbc96fd8ffcff7603e07b6
Jesse Liberty <JesseLiberty@non.se.com>
2021-02-22 12:59:23 -0500

    add converter skeleton

---
+++ Panofy/Panofy/Converter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PanofyB
+{
+    public class Converter
+    {
+        public double ConvertFtoC(double fahrenheitTemp)
+        {
+            // todo, implement method
+            return 0.0;
+        }
+    }
+}
