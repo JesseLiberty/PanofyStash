da77c91258a24c40caa902b74e71183577d4876d
Jesse Liberty <JesseLiberty@non.se.com>
2021-02-22 09:39:42 -0500

    First use of Panofy in Dir B

---
+++ Panofy/Panofy/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Panofy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Are we the same?");
+        }
+    }
+}
